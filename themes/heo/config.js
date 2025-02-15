d731d7ce6fef63a267bc9639c8303ff2b0309123
Lucas215zz <153696538+Lucas215zz@users.noreply.github.com>
2025-02-15 23:57:16 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到趣派跨境电商学苑社区', url: 'https://notion-next-chi-weld.vercel.app/' },
+    { title: '访问文档中心获取更多帮助', url: 'https://notion-next-chi-weld.vercel.app/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -22,7 +22,7 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '提升思维认知',
   HEO_HERO_TITLE_3: 'SZKJDSXY.COM',
   HEO_HERO_TITLE_4: '视频带货线上课',
-  HEO_HERO_TITLE_5: '忙活一月收益几十块？2025年这堂课带你从 0 到 1 做TikTok视频带货',
+  HEO_HERO_TITLE_5: '2025年这堂课带你从 0 到 1 做TikTok视频带货',
   HEO_HERO_TITLE_LINK: 'https://szkjdsxy.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '运营干货', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://notion-next-chi-weld.vercel.app/',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
