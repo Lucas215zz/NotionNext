72e9a2c5920871738fd2bee94291396862dfd680
Lucas215zz <153696538+Lucas215zz@users.noreply.github.com>
2025-02-23 13:31:26 +0800

    Update blog.config.js
    + KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'tiktok,,视频带货,tiktok视频带货,趣派跨境,趣派跨境电商学苑', // ← 加上逗号

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '趣派跨境', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '陪跑跨境企业，实现营业额倍增，让TikTok带货变得更高效', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.qpkjdsxy.com/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'tiktok,,视频带货,tiktok视频带货,趣派跨境,趣派跨境电商学苑'// 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'tiktok,视频带货,tiktok视频带货,趣派跨境,趣派跨境电商学苑',// 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
